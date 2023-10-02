baca7455bc7dc1dc8588e1c524636a9fab63c07d
Ray <146782753+rayx05@users.noreply.github.com>
2023-10-02 18:43:08 -0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 18:41:24 2023
+
+@author: rayxiong
+"""
+
+def area_rectangles(length,width):
+    return print ('make a change')
